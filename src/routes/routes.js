9de4faaa628f5abb0679fc8e731a84522bafd06e
MarianaArissa123 <arissamariana66@gmail.com>
2025-05-16 10:36:36 -0300

    criaçao das routes pessoais

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,8 @@
 const express = require('express'); 
 const router = express.Router(); 
 
-const autoresController = require('../controllers/autores'); 
-const generosController = require('../controllers/generos'); 
+import RotasMariana from './routes-mariana';
 
-router.get('/autores', autoresController.listarAutores); 
-router.post('/autores', autoresController.cadastrarAutores); 
-router.patch('/autores/:id', autoresController.editarAutores); 
-router.delete('/autores/:id', autoresController.apagarAutores); 
-
-router.get('/generos', generosController.listarGeneros); 
-router.post('/generos', generosController.cadastrarGeneros); 
-router.patch('/generos/:id', generosController.editarGeneros); 
-router.delete('/generos/:id', generosController.apagarGeneros); 
-
-
+router.use('/',RotasMariana);
 
 module.exports = router;
